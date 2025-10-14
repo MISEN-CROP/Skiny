f26df54c9d9dec17986a7e49dd5315898bd34825
Your Name <truongsonpt.80@gmail.com>
2025-10-14 11:41:04 +0700

    update

--- assets/config.js
+++ assets/config.js
@@ -5,3 +5,7 @@ const autoplaySpeed = 3;
 if (window.location.pathname === "/") {
     document.body.classList.add("template-index")
 }
+
+if (window.location.pathname.includes("/products/")) {
+    document.querySelector("header").classList.add("header-bg-second");
+}
