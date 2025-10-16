23d1cc4e546982e2032a6e286bcf4497527a8dcd
Your Name <truongsonpt.80@gmail.com>
2025-10-16 18:02:50 +0700

    update

--- assets/config.js
+++ assets/config.js
@@ -6,6 +6,6 @@ if (window.location.pathname === "/") {
     document.body.classList.add("template-index")
 }
 
-if (window.location.pathname.includes("/products/") || window.location.pathname.includes("/pages/") || window.location.pathname.includes("/collections/")) {
+if (window.location.pathname.includes("/products/") || window.location.pathname.includes("/pages/") || window.location.pathname.includes("/collections/") || window.location.pathname.includes("/search")) {
     document.querySelector("header").classList.add("header-bg-second");
 }
