138336e7fe6cdda8f8b6c07b72599b920583fd07
Your Name <truongsonpt.80@gmail.com>
2025-10-17 11:38:02 +0700

    update

--- assets/config.js
+++ assets/config.js
@@ -9,3 +9,26 @@ if (window.location.pathname === "/") {
 if (window.location.pathname.includes("/products/") || window.location.pathname.includes("/pages/") || window.location.pathname.includes("/collections/") || window.location.pathname.includes("/search")) {
     document.querySelector("header").classList.add("header-bg-second");
 }
+
+const addContainerApp = () => {
+    console.log("addContainerApp called");
+    const listSparkContainer = document.querySelectorAll(".spark-carousal-root");
+    listSparkContainer.forEach((container) => {
+        const h2 = container.querySelector("h2");
+        if (container) {
+            container.style.paddingTop = "30px";
+            container.style.paddingBottom = "30px";
+        }
+        if (h2) {
+            h2.setAttribute("class", "");
+            const textContent = h2.textContent || h2.innerText;
+            h2.innerHTML = `
+            <span class="text-left text-3xl md:text-4xl lg:text-6xl font-bold uppercase tracking-tight mb-10 block">${textContent}</span>
+            `
+        }
+        container.classList.add("page-container");
+    });
+}
+setTimeout(() => {
+    addContainerApp();
+}, 2000)
